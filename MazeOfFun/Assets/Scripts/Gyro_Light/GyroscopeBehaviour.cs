c578f6846cf4d83969de419c8be6dac48dd6a393
Mioupytjh <47248442+Mioupytjh@users.noreply.github.com>
2022-04-07 10:16:42 +0200

    New input system, take one

--- MazeOfFun/Assets/Scripts/Gyro_Light/GyroscopeBehaviour.cs
+++ MazeOfFun/Assets/Scripts/Gyro_Light/GyroscopeBehaviour.cs
@@ -11,7 +11,6 @@ public class GyroscopeBehaviour : MonoBehaviour
     public float rollSpeed = 10;
     float MaxAngle = 35;
 
-    private Quaternion rotation;
 
     // Start is called before the first frame update
     void Start()
