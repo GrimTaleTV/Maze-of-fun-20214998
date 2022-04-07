60a9b6b5907c2a886eedb67f7a943e72dc7f4ab6
Mioupytjh <47248442+Mioupytjh@users.noreply.github.com>
2022-04-07 10:59:51 +0200

    wtf?

--- MazeOfFun/Assets/Scripts/Gyro_Light/GyroscopeBehaviour.cs
+++ MazeOfFun/Assets/Scripts/Gyro_Light/GyroscopeBehaviour.cs
@@ -1,31 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class GyroscopeBehaviour : MonoBehaviour
 {
 
-    private bool gyroEnabled;
-    private Gyroscope gyro;
+    private bool GyroEnabled;
+    private UnityEngine.Gyroscope Gyro;
 
     public float rollSpeed = 10;
-    float MaxAngle = 35;
+    float maxAngle = 35;
 
+    private Quaternion rot;
 
     // Start is called before the first frame update
     void Start()
     {
-
-
-        gyroEnabled = EnabledGyro();
+        GyroEnabled = EnabledGyro();
     }
 
     private bool EnabledGryo()
     {
         if (SystemInfo.supportGyroscope)
         {
-            gyro = Input.gyro;
-            gyro.enabled = true;
+            Gyro = Input.gyro;
+            Gyro.enabled = true;
             return true;
         }
 
@@ -36,8 +36,8 @@ public class GyroscopeBehaviour : MonoBehaviour
     private void Update()
     {
         Vector3 rotation = new Vector3(); ;
-        rotation.x = gyro.gravity.x * MaxAngle;
-        rotation.y = gyro.gravity.y * MaxAngle;
+        rotation.x = Gyro.attitude.rot.x * maxAngle;
+        rotation.y = Gyro.attitude.rot.y * maxAngle;
 
         transform.eulerAngles = rotation;
     }
